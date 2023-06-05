c8094319337db03ed777260e2106dabbeab9bbf8
Dawodu Bolaji <dawodubolaji189@gmail.com>
2023-06-05 23:56:31 +0100

    add

---
+++ server/routes/post.routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+//IMPORT ALL THE CONTROLLERS ...
+
+import { getAllPosts, getPostDetail, createPost, updatePost, deletePost, } from '../controlers/post.controller.js';
+
+const router = express.Router();
+
+router.route('/').get(getAllPosts);
+router.route('/:id').get(getPostDetail);
+router.route('/').post(createPost);
+router.route('/:id').patch(updatePost);
+router.route('/:id').delete(deletePost);
+
+export default router;
